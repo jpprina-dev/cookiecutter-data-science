43b469d54d6aff927ffd3f9a9b2dfcdcf9c9940d
jpprina-dev <jpprina@gmail.com>
2023-09-27 17:02:58 -0300

    restructuring src/

--- {{ cookiecutter.repo_name }}/src/stages/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/stages/make_dataset.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import click
-import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+
+from src.utils.logs import get_logger
 
 
 @click.command()
@@ -12,14 +13,11 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
+    logger = get_logger(__name__)
     logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
